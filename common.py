1912539aa660a18af0634633bb1e77cb50584881
neo <g602049338@icloud.com>
2019-10-17 03:04:27 +0800

    init

--- common.py
+++ common.py
@@ -6,7 +6,7 @@ from plugins.HYplugins.orm import Common
 class Images(Common, db.Model):
     """图片记录"""
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, nullable=False, comment='用户ID')
+    user_uuid = db.Column(db.String(32), nullable=False, comment='用户ID')
     url = db.Column(db.String(length=255), nullable=False, unique=True, comment='图片地址')
     genre = db.Column(db.String(length=10), nullable=False, comment='图片用途')
 
