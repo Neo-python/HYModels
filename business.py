84276a8c45427df8cd64bab6878123843f74b950
neo <g602049338@icloud.com>
2019-10-19 00:29:31 +0800

    orm 模型新增委托管理员

--- business.py
+++ business.py
@@ -62,8 +62,11 @@ class OrderEntrustBase(Common, db.Model):
     order_uuid = db.Column(db.String(24), db.ForeignKey('factory_order.order_uuid'), comment='订单编号')
     driver_uuid = db.Column(db.String(length=32, collation='utf8_bin'), nullable=False, index=True, comment='驾驶员UUID')
     entrust_status = db.Column(db.SMALLINT, default=0, comment='委托状态. -1:委托锁定,无法接受委托 0:未接受委托 1:已接受委托')
+    managers_uuid = db.Column(db.String(length=32, collation='utf8_bin'), db.ForeignKey('admin.uuid'), nullable=False,
+                              index=True, comment='委托订单管理员')
 
     order = db.relationship(OrderBase, foreign_keys=[order_uuid])
+    managers = db.relationship('AdminBase', foreign_keys=[managers_uuid])
 
     __table_args__ = (
         db.UniqueConstraint('order_uuid', 'driver_uuid', name='uix_order_uuid_driver_uuid'),
