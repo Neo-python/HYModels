af33aa0bfe62bbc48b89fe6b928268a4ca47af0f
Neo-python <g602049338@icloud.com>
2019-12-07 21:09:10 +0800

    联系人

--- business.py
+++ business.py
@@ -89,7 +89,7 @@ class DriverOrderBase(Common, OrderIdModel, db.Model, Coordinate):
     driver_uuid = db.Column(db.String(length=32, collation='utf8_bin'), db.ForeignKey('driver.uuid'), nullable=False,
                             comment='驾驶员UUID')
     factory_order_uuid = db.Column(db.String(24), db.ForeignKey('factory_order.order_uuid'), comment='订单编号')
-    contact = db.Column(db.String(length=6), default='', comment='联系人')
+    contact = db.Column(db.String(length=20), default='', comment='联系人')
     phone = db.Column(db.String(length=13), nullable=False, comment='手机号')
     description = db.Column(db.Text, comment='订单详情')
     images = db.Column(db.JSON, comment='订单图片')
