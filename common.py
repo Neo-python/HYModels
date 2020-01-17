e07f0746f64eb4be129da1626a897dce1fc6f8b2
Neo-python <g602049338@icloud.com>
2020-01-17 21:33:03 +0800

    驾驶员登录验证驾驶员状态

--- common.py
+++ common.py
@@ -6,7 +6,7 @@ class ImagesBase(Common, db.Model):
     """图片记录"""
     __tablename__ = 'images'
     id = db.Column(db.Integer, primary_key=True)
-    user_uuid = db.Column(db.String(32), nullable=False, comment='用户ID')
+    user_uuid = db.Column(db.String(40), nullable=False, comment='用户ID')
     url = db.Column(db.String(length=255), nullable=False, unique=True, comment='图片地址')
     genre = db.Column(db.String(length=10), nullable=False, comment='图片用途')
 
